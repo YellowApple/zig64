7a60320ab812a84faf97985a995cad9d6233724f
ryno <northrup@yellowapple.us>
2025-01-30 01:05:12 +0000

    Forgot to export public ISViewer functions/variables
    FossilOrigin-Name: e93360c9d0c60846cef8c1c1c6f397b58eb0cc565491ed9c5538602a4b0467a0

--- src/ISViewer.zig
+++ src/ISViewer.zig
@@ -3,14 +3,14 @@ const PI = @import("./PI.zig");
 /// Writing to this register will cause the (emulated) IS-Viewer to
 /// read the specified number of bytes from `ISViewer.buffer` and
 /// display them in the emulator's text output.
-const write_len: *volatile u32 = @ptrFromInt(0xb3ff0014);
+pub const write_len: *volatile u32 = @ptrFromInt(0xb3ff0014);
 /// Buffer to store text to be sent via (emulated) IS-Viewer to the
 /// emulator.
-const buffer: *align(4) [0x200]u8 = @ptrFromInt(0xb3ff0020);
+pub const buffer: *align(4) [0x200]u8 = @ptrFromInt(0xb3ff0020);
 
 /// If true, an IS-Viewer (or an emulation thereof) is available.
 /// Otherwise, false.
-fn present() bool {
+pub fn present() bool {
     PI.wait();
     buffer[0] = 0x12;
     PI.wait();
@@ -18,7 +18,7 @@ fn present() bool {
 }
 
 /// Sends text via the (emulated) IS-Viewer.
-fn print(text: []const u8) void {
+pub fn print(text: []const u8) void {
     PI.writeBytes(buffer, text);
     PI.writeWord(write_len, text.len);
 }
